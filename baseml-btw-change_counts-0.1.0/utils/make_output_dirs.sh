af2cb56959543ba52e5ae53da9cb89b2a4a6ff8b
Haruka Yamashita <hyamashita@nig.ac.jp>
2025-05-30 14:52:45 +0900

    Remove the location of this folder on HY comoputer and add a comment that users need to put the folder path on their computers.

--- baseml-btw-change_counts-0.1.0/utils/make_output_dirs.sh
+++ baseml-btw-change_counts-0.1.0/utils/make_output_dirs.sh
@@ -4,20 +4,21 @@
 output_dir=$1
 
 # TODO: Need to generalize
-package=/Users/haruka/Documents/01_myPackages/shell/ppda
+# Please change this value to the location of this folder on your computer. 
+package_dir=/Users/<username>/<thisfolder>
 
 # Make output directory for alignments
 mkdir "$output_dir/1_alignment_processing"
 
 # Make output directory for BTW
 mkdir "$output_dir/2_BTW"
-cp "$package/btw/____codes.zip" "$output_dir/2_BTW"
+cp "$package_dir/btw/____codes.zip" "$output_dir/2_BTW"
 unzip -q "$output_dir/2_BTW/____codes.zip" -d "$output_dir/2_BTW"
 
-cp "$package/btw/_AI_for_collapse.zip" "$output_dir/2_BTW"
+cp "$package_dir/btw/_AI_for_collapse.zip" "$output_dir/2_BTW"
 unzip -q "$output_dir/2_BTW/_AI_for_collapse.zip" -d "$output_dir/2_BTW"
 
-cp "$package/btw/run_BTW_pipeline.sh" "$output_dir/2_BTW"
+cp "$package_dir/btw/run_BTW_pipeline.sh" "$output_dir/2_BTW"
 
 mkdir "$output_dir/2_BTW/_seq_list_to_be_concatenated"
 mkdir "$output_dir/2_BTW/_seqs_folder"
